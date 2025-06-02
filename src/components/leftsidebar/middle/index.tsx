a6c9ecbab78e4527399a8b2ebc500656b2b0170a
humbejeff2116 <humbejeff2116@gmail.com>
2025-06-02 07:40:37 +0100

    refactor to attach student id to link params for course reg route path

--- src/components/leftsidebar/middle/index.tsx
+++ src/components/leftsidebar/middle/index.tsx
@@ -1,15 +1,39 @@
+'use client';
 import linksService from '@/data/links';
 import NavigationLink from '../navLink';
 import styles from './index.module.css';
+import useAuth from '@/context/auth/context';
 
 const links = linksService.getSideNavLinks();
 
 export default function MiddleLeftSideBar() {
+    const { student } = useAuth();
+
     return (
         <div className={styles.container}>
-        {links.map((link, i) =>
-            <NavigationLink key={i} {...link}/>
-        )}
+            {/* {student && (
+                (() => {
+                    return <></>
+                })()
+            )} */}
+        {links.map((link, i) => {
+
+            if (link.name === "Course Reg") {
+                const newHref = `${link.href}/${student && encodeURIComponent(student.id || student._id)}`;
+
+                return (
+                    <NavigationLink 
+                    key={i} 
+                    {...link}
+                    href={newHref}
+                    />
+                )
+            }
+
+            return (
+                <NavigationLink key={i} {...link}/>
+            )
+        })}
         </div>
     )
 }
