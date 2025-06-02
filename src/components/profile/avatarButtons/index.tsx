fc8cbc342159738e783152aa9c9dc21f8c81ef57
humbejeff2116 <humbejeff2116@gmail.com>
2025-06-02 07:37:40 +0100

    clean code

--- src/components/profile/avatarButtons/index.tsx
+++ src/components/profile/avatarButtons/index.tsx
@@ -2,16 +2,17 @@ import Link from 'next/link';
 import styles from './index.module.css';
 import { IconContext } from 'react-icons';
 import { FiArrowRight } from 'react-icons/fi';
+import appRoutes from '@/routes';
 
 
 
 export default function AvatarLinks() {
     return (
         <div className={styles.container}>
-            <Link href={'/home/course-form'}>
+            <Link href={appRoutes.courseForm}>
             Print Course Form
             </Link>
-            <Link href={'/home/profile'}>
+            <Link href={appRoutes.profile}>
             View Profile
             <IconContext.Provider value={{className: styles.buttonIcon}}>
                 <FiArrowRight/>
