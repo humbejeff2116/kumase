5fd97da653ffb453bf781579b6199973c143cb0a
humbejeff2116 <humbejeff2116@gmail.com>
2025-06-25 07:59:30 +0100

    refactor getStudentsSlugs functionality

--- src/lib/students.ts
+++ src/lib/students.ts
@@ -1,5 +1,7 @@
 import { getStudentsSlugs } from "@/services/services.http";
 import { Course, getAllCourses } from "../data/courses";
+import fs from 'node:fs';
+import path from "node:path";
 
 class Service {
     courses = getAllCourses();
@@ -16,18 +18,29 @@ class Service {
      * NOTE: no error handling for this async function, please wrap 
      * in try catch and handle error when calling this function
      */
-    async getStudentsSlugs(studentId?: string) {
+    async getStudentsSlugs(studentId?: string): Promise<Array<any>> {
         try {
-            const { data:students } = await getStudentsSlugs(studentId);
+            const studentsFilePath = path.join(process.cwd(), 'src/data/students/students-lock.json');
+            // const { data:students } = await getStudentsSlugs(studentId);
+            return new Promise((res, rej) => {
+                if (!fs.existsSync(studentsFilePath)) {
+                    return res([]);
+                }
+                fs.readFile(studentsFilePath, 'utf-8', async (err, data) => {
+                    if (err) {
+                        rej(err)
+                    }
+                    const { allStudents:students } = JSON.parse(data);
+                    if (students && students.length > 0) {
+                        res(students.map((student: { _id: string }) => ({id: (student._id)})));
+                    }
+                    res([]);
+                });
+            }) 
 
-            if (students && students.length > 0) {
-                return students.map((student: { _id: string }) => ({id: (student._id)}));
-            }
-            return [];
         } catch (err) {
             console.error(err);
-            // TODO... return empty array
-            return [{id: 'hsjd'}];
+            return [];
         }
     }
 }
