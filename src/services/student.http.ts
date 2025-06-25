19a5cbd1287ac2b6e0649d25fd741538346f84b1
humbejeff2116 <humbejeff2116@gmail.com>
2025-06-25 08:03:47 +0100

    add getStudentAdmin, genStudentTokenAdmin, getStudentsAdmin, onboardStudentAdmin, registerStudentCourse API services

--- src/services/student.http.ts
+++ src/services/student.http.ts
@@ -1,3 +1,4 @@
+import { Student } from '../../scripts/students.types';
 import httpBase, { API_DOMAIN, httpBaseWithCredentials } from './http.config';
 
 export const APIBase = `${API_DOMAIN}/api/v1`;
@@ -5,12 +6,41 @@ const backendAPI = httpBase(APIBase);
 const backendAPIWithCredentials = httpBaseWithCredentials(APIBase);
 
 
-async function authStudentToken(studentId: string) {
-    const response = await backendAPI.post(`/student/token/auth`, studentId);
+async function authStudentToken(studentId: string, token: string) {
+    const response = await backendAPI.post(`/student/token/auth/${studentId}`, token);
     return response.data;
 }
 
+async function getStudentAdmin(idOrRegNo: string, queryType: 'id' | 'regNo') {
+    const response = await backendAPI.get(`/admin/student?id=${idOrRegNo}&queryType=${queryType}`);
+    return response.data;
+}
+
+async function getStudentsAdmin() {
+    const response = await backendAPI.get(`/admin/students`);
+    return response.data;
+}
+
+async function genStudentTokenAdmin(studentId: string) {
+    const response = await backendAPI.post(`/admin/student/token/${studentId}`);
+    return response.data;
+}
+
+async function onboardStudentAdmin(student: Student) {
+    const response = await backendAPI.post(`/admin/student/onboard/`, student);
+    return response.data;
+}
+
+async function registerStudentCourse(studentId: string, course: any) {
+    const response = await backendAPI.post(`/student/course/register/${studentId}`, course);
+    return response.data;
+}
 
 export {
     authStudentToken,
+    getStudentAdmin,
+    genStudentTokenAdmin,
+    getStudentsAdmin,
+    onboardStudentAdmin,
+    registerStudentCourse
 }
