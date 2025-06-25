b7024ef908bcb65ac49472c796edd4f95cae4ef5
humbejeff2116 <humbejeff2116@gmail.com>
2025-06-25 07:36:13 +0100

    remove hard coded mock data

--- src/components/courseReg/index.tsx
+++ src/components/courseReg/index.tsx
@@ -3,21 +3,6 @@ import { authenticateCourseReg, getStudent } from '@/services/services.http';
 import TokenAuth from './tokenAuth';
 
 
-
-
-// get student token from api for the session
-//check if student has registered course,
-// if student has registered course,
-//display the course form component
-//if student has not registered course,
-// display the token authentication form
-//
-
-
-//if user has a token, display the token
-//user enters token in form
-// collect token
-// if no active token, d
 interface CourseRegprops {
     studentId: string
 }
@@ -25,31 +10,8 @@ export default async function CourseReg({
     studentId
 }: CourseRegprops) {
     try {
-        // const { isRegistered } = await authenticateCourseReg(studentId);
-        // const { studentToken, student } = await getStudent(studentId);
-
-        const { isRegistered } = {
-            isRegistered: false
-        }
-        const { studentToken, student } = {
-            studentToken: {
-                _id: "string",
-                studentId: "hsjd",
-                value: "A99bc_b99hyHGf",
-                session: "string",
-                semester: "string",
-                purchaseTime: "string",
-            },
-            student: {
-                _id: 'hsjd',
-                id: 'hsjd',
-                surname: 'John',
-                firstName: 'Doe',
-                regNo: 'dt/2012/177',
-                level: '100'
-
-            }   
-        };
+        const { isRegistered } = await authenticateCourseReg(studentId);
+        const { studentToken, data:student } = await getStudent(studentId, 'id');
         
         return (
             <Suspense fallback={<div>Loading...</div>}>
@@ -61,6 +23,12 @@ export default async function CourseReg({
             </Suspense>
         )
     } catch (err) {
-        console.error('error occured while getting student', err);
+        console.error('error occured while rendering CouresReg component');
+        return (
+        // TODO... display a proper error component
+            <div>
+               Oppps!!! (An Error has  occured)!!!
+            </div>
+        )
     }
 }
