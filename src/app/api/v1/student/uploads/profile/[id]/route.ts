cab1805cea0b23e0486f82401fb4e36c812f178e
humbejeff2116 <humbejeff2116@gmail.com>
2025-07-07 13:35:33 +0100

    rename api/v1/student/uploads/profile/\[studentId\]/\[accountId\] -> api/v1/student/uploads/profile/\[id\]/

--- src/app/api/v1/student/uploads/profile/[id]/route.ts
+++ src/app/api/v1/student/uploads/profile/[id]/route.ts
@@ -3,13 +3,16 @@ import { NextRequest, NextResponse } from "next/server";
 
 
 interface Params {
-    params: Promise<{
-        accountId: string;
-        studentId: string;
-    }>
+    params: Promise<{ id: string }>
 }
 
 
+// Disable Next.js body parsing to handle file uploads
+// export const config = {
+//     api: {
+//         bodyParser: false,
+//     },
+// };
 export async function POST(req: NextRequest, res: NextResponse, params: Params) { 
     return studentController.updateProfileImage(req, params);
 }
