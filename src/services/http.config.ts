8f23669ebc1fdf85bc2417f3ec90c9f61167e5c0
humbejeff2116 <humbejeff2116@gmail.com>
2025-07-07 13:44:40 +0100

    add APIBase2

--- src/services/http.config.ts
+++ src/services/http.config.ts
@@ -3,6 +3,7 @@ import axios from 'axios';
 export const API_DOMAIN = `${process.env.NEXT_PUBLIC_API_URL}/v1` || `http://localhost:3000/api`;
 export const IMAGE_DOMAIN = `${process.env.NEXT_PUBLIC_API_URL}/v1` || `http://localhost:3000/api`;
 export const APIBase = `${process.env.NEXT_PUBLIC_API_URL}/v1`;
+export const APIBase2 = `${process.env.NEXT_PUBLIC_API_URL}/v2`;
 export default function httpBase(URL: string, contentType = "application/json") {
     return axios.create({
         baseURL: URL,
@@ -28,4 +29,5 @@ export function httpBaseWithCredentials(URL: string, contentType = "application/
 }
 
 export const backendAPI = httpBase(APIBase);
+export const backendAPIV2 = httpBase(APIBase2);
 export const backendAPIWithCredentials = httpBaseWithCredentials(APIBase);
