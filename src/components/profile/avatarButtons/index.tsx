1da08da723f5d8f174534bee6c475af5f19a29aa
humbejeff2116 <humbejeff2116@gmail.com>
2025-07-08 11:51:20 +0100

    refactor AvatarLinks comp to properly return student account and course form links

--- src/components/profile/avatarButtons/index.tsx
+++ src/components/profile/avatarButtons/index.tsx
@@ -1,22 +1,29 @@
+'use client'
 import Link from 'next/link';
 import styles from './index.module.css';
 import { IconContext } from 'react-icons';
 import { FiArrowRight } from 'react-icons/fi';
 import appRoutes from '@/routes';
+import useAuth from '@/context/auth/context';
 
 
 
 export default function AvatarLinks() {
+    const { student } = useAuth();
+    const accountLink = `${appRoutes.account}/${student && encodeURIComponent(student.id || student._id)}`;
+    const courseFormLink = `${appRoutes.courseForm}/${student && encodeURIComponent(student.id || student._id)}`;
+    
     return (
         <div className={styles.container}>
-            <Link href={appRoutes.courseForm}>
-            Print Course Form
+            <Link href={courseFormLink}>
+                Course Form
             </Link>
-            <Link href={appRoutes.profile}>
-            View Profile
-            <IconContext.Provider value={{className: styles.buttonIcon}}>
-                <FiArrowRight/>
-            </IconContext.Provider>
+            
+            <Link href={accountLink}>
+                My Account
+                <IconContext.Provider value={{className: styles.buttonIcon}}>
+                    <FiArrowRight/>
+                </IconContext.Provider>
             </Link>
         </div>
     )
